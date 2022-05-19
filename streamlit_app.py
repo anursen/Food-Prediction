3fdb70b98cf5e3fe676963ae6224b211d4af8d9c
anursen <96952461+anursen@users.noreply.github.com>
2022-05-18 21:38:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+# TF2 version
+import tensorflow.compat.v2 as tf
+import tensorflow_hub as hub
+import cv2
+from skimage import io
+
 
 st.text('Welcome')
 st.text('Please upload your Amazon data')
@@ -8,5 +14,31 @@ st.text('Please upload your Amazon data')
 
 try:
     uploaded_file = st.file_uploader(label = 'Upload Your File', type = ['csv', 'xlsx'])
+
+
+    cake_url = path
+labelmap_url = "https://www.gstatic.com/aihub/tfhub/labelmaps/aiy_food_V1_labelmap.csv"
+input_shape = (224, 224)
+
+m = hub.KerasLayer('https://tfhub.dev/google/aiy/vision/classifier/food_V1/1')
+image = np.asarray(io.imread(cake_url), dtype="float")
+image = cv2.resize(image, dsize=input_shape, interpolation=cv2.INTER_CUBIC)
+# Scale values to [0, 1].
+image = image / image.max()
+# The model expects an input of (?, 224, 224, 3).
+images = np.expand_dims(image, 0)
+# This assumes you're using TF2.
+output = m(images)
+predicted_index = output.numpy().argmax()
+classes = list(pd.read_csv(labelmap_url)["name"])
+
+st.text("Prediction: ", classes[predicted_index])
+    
+    
+    
+    
+    
+    
+    
 except:
     print('yes')
